67c693de8fca7464a887a8a559c07f4c468784ab
labo <labo@algo.ai>
2024-11-03 10:47:00 +0100

    please clean your code

--- src/main/java/springgradelproject/BookAuthorCRUDApiProjectApplication.java
+++ src/main/java/springgradelproject/BookAuthorCRUDApiProjectApplication.java
@@ -3,6 +3,7 @@ package springgradelproject;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// TODO: Please clean you code
 @SpringBootApplication
 public class BookAuthorCRUDApiProjectApplication {
 
